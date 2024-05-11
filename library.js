e75c316f58178cd93aefe607415747b5e7d14522
tsfoverlord <rat4521@gmail.com>
2024-05-11 07:13:14 +0530

    Add Book constructor

--- library.js
+++ library.js
@@ -1,1 +1,10 @@
-console.log('hello')
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        let readingStatus = this.read ? "read" : "not read yet" 
+        return `${this.title} by ${this.author}, ${this.pages} pages,${readingStatus}`
+    }
+}   
