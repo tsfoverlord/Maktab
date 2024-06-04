508d7d8a84fad821154cab31e4266fbcb5c3ba0b
tsfoverlord <rat4521@gmail.com>
2024-06-04 18:04:43 +0530

    Implement delete books and fix displayBooks

--- library.js
+++ library.js
@@ -9,46 +9,46 @@ function Book(title, author, pages, read) {
     }
 }   
 
-const myLibrary = []
-const sampleBook = new Book('A fighting man of Mars','Edgar Rice Burroughs',138,false);
-myLibrary.push(sampleBook);
+const myLibrary = [];
+// const sampleBook = new Book('A fighting man of Mars','Edgar Rice Burroughs',138,false);
+// myLibrary.push(sampleBook);
 
 const shelf = document.querySelector('.shelf');
 
 
 function displayBooks() {
-    if(!myLibrary.length){
-        return;
+    //Remove previously rendered books and re-render
+    shelf.innerHTML = "";
+    for(let i = 0; i < myLibrary.length; i++){
+        book = myLibrary[i];
+        const title = document.createElement('h1');
+        title.textContent = book.title;
+        const author = document.createElement('h2');
+        author.textContent = book.author;
+        const pages = document.createElement('h3');
+        pages.textContent = book.pages + " pages";
+        const readStatus = document.createElement('p');
+        readStatus.textContent = book.read ? "Read" : "Not read yet";
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click',deleteBook);
+        const bookInfo = document.createElement('div');
+        bookInfo.classList.add('book-info');
+        bookInfo.append(title,author,pages,readStatus);
+
+        const bookdiv = document.createElement('div');
+        bookdiv.classList.add('book');
+        bookdiv.dataset.index = i;        
+        bookdiv.append(bookInfo); 
+        bookdiv.append(deleteButton);        
+        shelf.append(bookdiv);
     }
-
-    const book = myLibrary[myLibrary.length - 1];
-
-    const title = document.createElement('h1');
-    title.textContent = book.title;
-    const author = document.createElement('h2');
-    author.textContent = book.author;
-    const pages = document.createElement('h3');
-    pages.textContent = book.pages + " pages";
-    const readStatus = document.createElement('p');
-    readStatus.textContent = book.read ? "Read" : "Not read yet";
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    const bookInfo = document.createElement('div');
-    bookInfo.classList.add('book-info');
-    bookInfo.append(title,author,pages,readStatus);
-
-    const bookdiv = document.createElement('div');
-    bookdiv.classList.add('book');
-    bookdiv.dataset.index = myLibrary.length - 1;        
-    bookdiv.append(bookInfo); 
-    bookdiv.append(deleteButton);        
-    shelf.append(bookdiv);
 
 }
 
 const dialog = document.querySelector('dialog');
 const dialogClose = document.querySelector('dialog .close');
-const form = document.querySelector('dialog form')
+const form = document.querySelector('dialog form');
 
 dialogClose.addEventListener('click', (e) => {
     e.preventDefault();
@@ -73,3 +73,9 @@ function createBookFromForm(e) {
 }
 
 form.addEventListener('submit', createBookFromForm);
+
+function deleteBook(e) {
+    const index = e.target.parentNode.dataset.index;
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
