7a86fd6e0e2c54a418f237f1f2f625483fd9218f
tsfoverlord <rat4521@gmail.com>
2024-06-26 19:09:16 +0530

    Refactor: Use class syntax

--- library.js
+++ library.js
@@ -1,13 +1,16 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
+    info() {
         let readingStatus = this.read ? "read" : "not read yet" 
         return `${this.title} by ${this.author}, ${this.pages} pages,${readingStatus}`
     }
-}   
+}
 
 const myLibrary = [];
 // const sampleBook = new Book('A fighting man of Mars','Edgar Rice Burroughs',138,false);
